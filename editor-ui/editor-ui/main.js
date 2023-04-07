8eba2513b0785b3d6fa543e1c651d7372b314c49
Ihor Babaian <ihor.babaian@outlook.com>
2023-04-07 10:48:26 +0300

    Finish initialize

--- editor-ui/editor-ui/main.js
+++ editor-ui/editor-ui/main.js
@@ -5,22 +5,23 @@ const isMac = process.platform === "darwin";
 const isDev = process.env.NODE_ENV === "development";
 
 function createWindow() {
-  process.env['ELECTRON_DISABLE_SECURITY_WARNINGS'] = 'true';
+  process.env["ELECTRON_DISABLE_SECURITY_WARNINGS"] = "true";
   win = new BrowserWindow({
     title: "Last Epoch save editor",
     width: isDev ? 1000 : 800,
-    height: 600,
+    height: isDev ? 1000 : 600,
     icon: `${__dirname}/dist/editor-ui/assets/icon.png`,
     webPreferences: {
       nodeIntegration: true
     }
   });
 
+  win.removeMenu();
   if (isDev)
     win.webContents.openDevTools();
 
   win.loadFile(`${__dirname}/dist/editor-ui/index.html`);
-  win.on('closed', () => {
+  win.on("closed", () => {
     win = null;
   });
 }
